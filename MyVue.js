b6a1af3772c3dd9692b1980aad028fdda86f1269
caren <caren_szu@foxmail.com>
2018-11-26 21:12:10 +0800

    实现从页面到数据的单向绑定( 即对 <input> 元素的修改会反映到数据上 )

--- MyVue.js
+++ MyVue.js
@@ -1,6 +1,5 @@
 class MyVue {
   constructor(options) {
-    
     let { el, data } = options;
     this._options = options;
     this.$el = document.querySelector(el);
@@ -39,31 +38,45 @@ class MyVue {
 
   compile() {
     let fragment = document.createDocumentFragment();
-    let child,
-      vm = this;
+    let child;
+    let vm = this;
     while ((child = vm.$el.firstChild)) {
       fragment.appendChild(child);
     }
+
     replace(fragment);
     vm.$el.appendChild(fragment);
 
     function replace(fragment) {
       Array.from(fragment.childNodes).forEach(node => {
-        let txt = node.textContent;
         let reg = /\{\{(.*)\}\}/g;
 
-        // txt: '{{ bestFriend.name }} '
-        // RegExp.$1: ' bestFriend.name '
-        if (node.nodeType == 3 && reg.test(txt)) {
-          let exp = RegExp.$1.trim();
-          let expArr = exp.split(".");
-          let value;
-          for(let e of expArr){
-            value = vm[e];
-          }
-          console.log(value);
-          node.textContent = value;
-          // txt = value;
+        if (node.nodeType == 3 && reg.test(node.textContent)) {
+          // 文字节点，并且文字内容包含双大括号 ;
+          let exp = RegExp.$1;
+          let value = vm._getValueByExpressionString(exp);
+          node.textContent = node.textContent.replace(reg, value);
+          // TODO: add watcher
+        } else if (node.nodeType == 1) {
+          // 元素节点
+          Array.from(node.attributes).forEach(attr => {
+            let name = attr.name;
+            let exp = attr.value;
+
+            if (name.startsWith("v-") || name.startsWith(":")) {
+              let value = vm._getValueByExpressionString(exp);
+              node.value = value;
+
+              // 双向绑定，页面 ==> 数据
+              node.addEventListener("input", e => {
+                let newVal = e.target.value;
+                console.log(exp, newVal);
+                vm._setValueByExpressionString(exp, newVal);
+              });
+            }
+
+            // TODO: add watcher
+          });
         }
 
         if (node.childNodes && node.childNodes.length) {
@@ -71,21 +84,41 @@ class MyVue {
         }
       });
     }
+  }
 
-    // // 尝试直接 replace fragment，这是我犯的一个错。
-    // function replace(fragment) {
-    //   let txt = fragment.textContent;
-    //   let reg = /\{\{(.*)\}\}/g;
-    //   console.log(typeof txt);
-    //   txt = txt.replace(reg, exp => {
-    //     let value;
-    //     exp = exp.trim().split(".");
-    //     for (let e of exp) {
-    //       value = vm[e];
-    //     }
-    //     console.log(value);
-    //     return value;
-    //   });
-    // }
+  /**
+   *
+   * set data according to expression:
+   *
+   * @param {String} exp
+   * @param {*} val
+   * @memberof MyVue
+   */
+  _setValueByExpressionString(exp, val) {
+    let expArr = exp.trim().split(".");
+    let data = this.data;
+
+    for (let i = 0; i < expArr.length - 1; i++) {
+      data = data[expArr[i]];
+    }
+    data[expArr[expArr.length - 1]] = val;
+  }
+  /**
+   *
+   *  return the value of expression.
+   *  for example:
+   *  "bestFriend.name" --> this.bestFriend.name
+   *
+   * @param {String} exp
+   * @returns the value of {{exp}}
+   * @memberof MyVue
+   */
+  _getValueByExpressionString(exp) {
+    let expArr = exp.trim().split(".");
+    let value = this;
+    for (let e of expArr) {
+      value = value[e];
+    }
+    return value;
   }
 }
