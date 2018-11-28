13f909c33e8efe87aba8329cb40d9edfe13da416
caren <caren_szu@foxmail.com>
2018-11-28 17:41:20 +0800

    将 TemplateCompiler、Publisher、Subscriber、MyVue 四个类分成不同文件

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
-let app = new MyVue({
-  el: "#app",
-  data: function() {
-    return {
-      name: "Christina",
-      age: 21,
-      height: 167,
-      description: "An insteresting girl",
-      bestFriend: {
-        name: "Caren",
-        age: 12,
-        link: "http://laijiawen.com/",
-      }
-    };
-  },
-  computed: {
-    // 十年后的年龄 （= =）
-    ageAfter10Years(){
-      return this.age + 10;
+let app;
+function start() {
+  app = new MyVue({
+    el: "#app",
+    data: function() {
+      return {
+        name: "Christina",
+        age: 21,
+        height: 167,
+        description: "An insteresting girl",
+        bestFriend: {
+          name: "Caren",
+          age: 12,
+          link: "http://laijiawen.com/"
+        },
+        不要点我:"你不乖哦:)" // 皮这一下觉得很开心
+      };
     },
-
-    // 二十年后的年龄 (= =)
-    ageAfter20Years:{
-      get(){
-        return this.age + 20;
+    computed: {
+      // 十年后的年龄 （= =）
+      ageAfter10Years() {
+        return this.age + 10;
       },
-      set(val){
-        // 二十年后的年龄变了，说明现在的年龄也变了
-        this.age = val - 20;
+
+      // 二十年后的年龄 (= =)
+      ageAfter20Years: {
+        get() {
+          return this.age + 20;
+        },
+        set(val) {
+          // 二十年后的年龄变了，说明现在的年龄也变了
+          this.age = val - 20;
+        }
       }
     }
-  }
-});
+  });
+}
